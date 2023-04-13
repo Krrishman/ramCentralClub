2e11458ba8fffef89ee64b3a69e8a3a95765b9f5
Krrishman <you@example.com>
2023-04-13 01:06:55 -0400

    uplooad

--- About.php
+++ About.php
@@ -253,6 +253,61 @@ echo "</table><br>  dfdfdfssdfsdf
 
 //pg_close($conn);
 
-?>
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $file = $_FILES['image'];
+    $file_name = $file['name'];
+    $file_tmp = $file['tmp_name'];
+  
+    // Upload file to Supabase storage bucket
+    $supabase_url = 'https://albvpiascovyowczwqez.supabase.co';
+    $supabase_bucket_name = 'clubpic';
+    $supabase_api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsYnZwaWFzY292eW93Y3p3cWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcwNDk1MzcsImV4cCI6MTk5MjYyNTUzN30.GPMlnXEDxeIjFcPw9IrJTkxzSc8QhC4kbwWXJpeaPPQ';
+    
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $supabase_url . '/storage/v1/upload/' . $supabase_bucket_name . '/' . $file_name);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, file_get_contents($file_tmp));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/octet-stream',
+      'Authorization: Bearer ' . $supabase_api_key
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    $file_url = json_decode($response)->data->url;
+    
+    
+    $query = "INSERT INTO images (file_name, file_url) VALUES ('$file_name', '$file_url')";
+    $result = pg_query($conn, $query);
+    
+    if ($result) {
+      echo "Image uploaded and metadata stored successfully!";
+    } else {
+      echo "Failed to upload image or store metadata: " . pg_last_error();
+    }
+    
+  }
+  ?>
+  
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Image Upload</title>
+  </head>
+  <body>
+    <h1>Image Upload</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+      <input type="file" name="image">
+      <input type="submit" value="Upload">
+    </form>
+  </body>
+  </html>
+
+
+
+
+
+
 
 
