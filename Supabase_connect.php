3eb442e12c273fc474a617778353d51e310b5e0c
Krrishman <you@example.com>
2023-04-25 05:15:53 -0400

    fdfgrgrgf

--- Supabase_connect.php
+++ Supabase_connect.php
@@ -11,9 +11,7 @@
     $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
     // Check connection
-    echo "fffffffffff";
     if (!$conn) {
-      echo "failllled";
       echo "PostgreSQL Connection Failure: " . pg_last_error();
       //exit;
        // die("Connection failed: " . pg_last_error());
