7494fa27940fcfef54d714030fef2bb52cbd5826
Krrishman <you@example.com>
2023-04-25 05:13:11 -0400

    bfbfbfbfb

--- Club.php
+++ Club.php
@@ -8,7 +8,6 @@
     include('session.php');
 	include('menubar.php');
     include('Supabase_connect.php');
-    //include('FSC_connect.php');
 
 ?>
 
@@ -34,151 +33,6 @@
                 <option value="orange">Relevent </option>
                 </select>
             </div>
-        <div class="club_Grid">
-            <div class="club_Container">
-                <div class="image_Container">
-                    <img class="club_Icon" src="ClubPictures/Psychology club.jpeg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                    <div class="information">
-                        <h1 ><a style="text-decoration:none; color:white;" href="./club_home_page.php">Psychology Club</a></h1>
-                        <p >Provide an opportunity for students to learn about the professions of psychology and their varied practices.</p>
-                    </div>
-                <div class="member_Counter">
-                    <h1 class="counter_Text"><i class="fas fa-user-friends"></i> Members: 46</h1>
-                </div>
-            </div>
-            
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="ClubPictures/Art & Design Club.jpeg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Art & Design Club/ClubHomePage.html">Art & Design Club</a></h1>
-                    <p >A club where students can not only express themselves, but bring life and vibrancy to campus.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 67</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Disability Coalition Club/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1><a style="text-decoration:none; color:white;" href="./All Club Page/Disability Coalition Club/ClubHomePage.html">Disability Club</a></h1>
-                    <p >learn from one another and use our shared knowledge to explore new ideas, create new opportunities, and develop relationships we can carry into the future.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 124</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Environmental Club/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1><a style="text-decoration:none; color:white;" href="./All Club Page/Environmental Club/ClubHomePage.html">Environmental Club</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 51</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="ClubPictures/Supporting Women in Computing.jpeg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Supporting Women in Computing/ClubHomePage.html">SWiC Club</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Farmingdale Esports/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Farmingdale Esports/ClubHomePage.html">Farmingdale Esports</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Gaming Club/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Gaming Club/ClubHomePage.html">Gaming Club</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Institute of Electrical/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Institute of Electrical/ClubHomePage.html">Institute of Electrical</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/M.S.A/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/M.S.A/ClubHomePage.html">M.S.A</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-            <div class="club_Container" >
-                <div class="image_Container">
-                    <img class="club_Icon" src="./All Club Page/Volleyball Club General Practice/picture/0.jpg" alt="">
-                </div>
-                <!--MAKE SURE THERE'S A WORD LIMIT TO WHAT CLUB DESCRIPTION CAN HAVE!!!-->
-                <div class="information">
-                    <h1 ><a style="text-decoration:none; color:white;" href="./All Club Page/Volleyball Club General Practice/ClubHomePage.html">Volleyball Club</a></h1>
-                    <p >Goal is to make the community aware of women, minorities and student talent in the computing industry.</p>
-                </div>
-                <div class="member_Counter">
-                    <h1 class="counterText"><i class="fas fa-user-friends"></i> Members: 18</h1>
-                </div>
-            </div>
-
-
-        </div>
-        
-
 <?php
 
 $cat	= array('club_id', 'made_date', 'c_members');
@@ -220,16 +74,6 @@ while ($row = pg_fetch_assoc($result)) {
     $c_pic = $row['c_pic'];
     $c_members = $row['c_members'];
     $Date = $row['made_date'];
-
-	echo "<tr>
-		<td align='center'>$club_id</td>
-		<td align='left'>$c_name</td>
-		<td align='center'>$c_tag</td>
-		<td align='center'>$c_desc</td>
-        <td align='left'>$c_pic</td>
-		<td align='center'>$c_members</td>
-		<td align='center'>$Date</td>
-		</tr>"; 
         
         echo"<div class='club_Container'>
         <div class='image_Container'>
