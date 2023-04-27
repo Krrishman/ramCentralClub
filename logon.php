ccab41ea09d3978e7cbd0f180226513dad3a3557
Krrishman <you@example.com>
2023-04-27 04:23:30 -0400

    cscscxcsc

--- logon.php
+++ logon.php
@@ -45,7 +45,7 @@ if (isset($_POST['click'])) {   $click = trim($_POST['click']);
 
 // Verify Input
 if (isset($_POST['register'])){
-
+	echo"kk $domain";
 	if ($f_name == NULL) 		{$msg = "First Name is missing<br>"; }
 	if ($l_name == NULL) 		{$msg = "Last Name is missing<br>"; }
 	if ($u_name == NULL) 		{$msg = "User Name is missing<br>"; }
@@ -76,9 +76,16 @@ if (isset($_POST['register'])){
 		if ($msg == NULL) {
 			
 // Query Student Using the user_name			
+			//include('bcs350_mysqli_connect.php');
+			//include('FSC_connect.php');
 			include('Supabase_connect.php');
 			
-
+			//$query = "SELECT account_number, account_type, pass_code, role 
+			//		  FROM bank 
+			//		  WHERE user_name = '$user_name'";
+			//$result = mysqli_query($mysqli, $query);
+		//	if (!$result) echo "Query Error [$query] " . mysqli_error($mysqli);
+		echo"gooodddd";
 // If user_name is FOUND, Verify pass_code		
 
 $query = 'SELECT * FROM "User" where "User_Name" = \'' . $user_name . '\'';
@@ -106,6 +113,10 @@ if (pg_num_rows($result) > 0) {
 	$Date = $row['created_at'];
 
 
+
+
+		//	if (mysqli_num_rows($result) > 0) {
+		//		list($User_id, $F_Name, $L_Name, $user_name, $Pass_Code2, $Role, $Year,$Major , $Email , $Phone, $Date, $Status) = mysqli_fetch_row($result);
 				
 // If pass_code matches, Complete LOGON				
 				if ($pass_code == $Pass_Code2) {
@@ -128,6 +139,17 @@ if (pg_num_rows($result) > 0) {
 	if ($msg == NULL)  	{$msg = "Enter User ID and pass_code";}
 	else if (!$logon){ $msg = "<font color='red'>$msg Please try again</font>";	}
 	include('menubar.php');
+	//include('header.php');
+/*	 echo "<p align='center'>Website Logon
+		  <p><form action='$pgm' method='post' autocomplete='off' align='center'>
+		  <table width='$width' align='center'>
+		  <tr><td $td>User ID</td>		<td $tf><input type='text'     name='user_name'   value='' size='60' maxlength='80' autocomplete='off' required></td></tr>
+		  <tr><td $td>pass_code</td>		<td $tf><input type='pass_code' name='pass_code' value='' size='12' maxlength='12' autocomplete='off' required></td></tr>
+		  </table>
+		  <p align='center'><input type='submit' name='logon' value='LOGON' style='background-color:lightgreen;font-weight:bold'>
+		  <p><table align='center' width='$width'><tr><td $td>Message</td><td $tf><b>$msg</b></td></tr></table>
+		  </form>"; */
+
 
 	echo"
 	
@@ -162,7 +184,7 @@ if (pg_num_rows($result) > 0) {
 	  <label ><b>Username</b></label>
 	  <input autocomplete='off' type='text' placeholder='Enter Username' name='u_name'   value='$u_name'>
 	  <label ><b>Password</b></label>
-	  <input autocomplete='off' type='number' placeholder='Enter Password' name='p_code'   value='$p_code'>
+	  <input autocomplete='off' type='text' placeholder='Enter Password' name='p_code'   value='$p_code'>
 	  <label>
 		<input type='checkbox' name='role' value='student'> Are You A Regular Student
 	  </label >
@@ -196,5 +218,80 @@ if (pg_num_rows($result) > 0) {
 	";
 	//include('footer.php');		  
 
+
+//include('test00.php');
+
+	//INSERT INTO `bank` (`account_number`, `user_name`, `pass_code`, `account_type`, `role`, `account_balance`, `date`) VALUES ('1001', 'dm', '2222', 'Checking', 'admin', '10000', '2022-12-12');
 ?>
 
+
+
+
+<!--
+<div id="myDIV" class="modal" style="display: none;">
+  
+  <form class="modal-content animate" action="home.php" method="post" autocomplete='off'>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name='user_name'   value='' autocomplete='off' required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="Password" placeholder="Enter Password" name='pass_code' value=''  autocomplete='off' required>
+        
+      <button type="submit"  name='logon' value='LOGON' >Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="myFunction()" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+<script src="click.js"></script>
+</body>
+</html>
+
+
+echo'
+	
+<div id="myDIV" class="modal">
+  
+<form class="modal-content animate" action="logon.php" method="post" >
+
+  <div class="container">
+	<label for="uname"><b>Username</b></label>
+	<input type="text" placeholder="Enter Username" name="user_name"   value=""  required>
+
+	<label for="psw"><b>Password</b></label>
+	<input type="Password" placeholder="Enter Password" name="pass_code" value=""  required>
+	  
+	<button type="submit"  name="logon" value="LOGON" >Login</button>
+	<label>
+	  <input type="checkbox" checked="checked" name="remember"> Remember me
+	</label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+	<button type="button" onclick="myFunction()" class="cancelbtn">Cancel</button>
+	<span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+  <div class="con" style="background-color:#f1f1f1">
+	<button type="button">Message</button>
+	<p> '; echo"$msg"; 
+	echo' </p>
+  </div>
+</form>
+</div>
+
+
+<script src="click.js"></script>
+</body>
+</html>
+	
+	
+	';
