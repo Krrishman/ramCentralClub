ac290557fcfe0cde2ce66a4e51ad25d1b9e5a87c
Krrishman <you@example.com>
2023-04-27 05:22:21 -0400

    dsfefdddddddvs

--- create_club.php
+++ create_club.php
@@ -9,8 +9,7 @@
     include('session.php');
     include('check_logon.php');
 	include('menubar.php');
-	include('FSC_connect.php');
-
+    include('Supabase_connect.php');
 
 ?>
 
@@ -21,9 +20,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link rel="stylesheet" href="create_club.css">
+        <link rel="stylesheet" href="SlideShow.css">
 		<link rel="stylesheet" href="footer.css">
     </head>
-
+    <style>
+        .color-box {
+            width: 50px;
+            height: 50px;
+            display: inline-block;
+            margin-right: 10px;
+            border: 1px solid #000;
+        }
+    </style>
     <body>
 <section>
 
@@ -31,11 +39,13 @@
 //$club_id, $c_name, $c_tag,$c_desc, $c_pic, $c_members, $made_by, $made_date
 $dd= date("Y-m-d");
 $i=1;
+$file_name =NULL;
 
 if (isset($_POST['task']))			$task = $_POST['task'];						else $task = "First";
 if (isset($_GET['r']))				{$task = $_GET['r'];}	
 if (isset($_POST['club_id']))			$club_id = trim($_POST['club_id']);	    else $club_id = NULL;
 if (isset($_POST['c_name']))			$c_name = trim($_POST['c_name']);       else $c_name = NULL;
+if (isset($_POST['color']))			$color = trim($_POST['color']);       else $color = NULL;
 if (isset($_POST['c_tag']))				$c_tag = trim($_POST['c_tag']);         else $c_tag = NULL;
 if (isset($_POST['c_desc']))			$c_desc = trim($_POST['c_desc']);       else $c_desc = NULL;
 //if (isset($_POST['c_pic']))			    $c_pic = trim($_POST['c_pic']);         else $c_pic = NULL;
@@ -43,10 +53,16 @@ if (isset($_POST['c_members']))			$c_members = trim($_POST['c_members']); else $
 if (isset($_POST['made_by']))			$made_by = trim($_POST['made_by']);     else $made_by = NULL;
 if (isset($_POST['made_date']))			$made_date = trim($_POST['made_date']); else $made_date = NULL;
 
-//if (isset($_POST['p_name']))			      $p_name = ($_POST['p_name']);     else $p_name = 0;
-//if (isset($_POST['p_desc']))			      $p_desc = ($_POST['p_desc']);     else $p_desc = 0;
-//if (isset($_POST['p_pic']))			          $p_pic = ($_POST['p_pic']);       else $p_pic = 0;
+if (isset($_POST['t_color1']))			$t_color1 = trim($_POST['t_color1']);      else $t_color1 = 'purple';
+if (isset($_POST['t_color2']))			$t_color2 = trim($_POST['t_color2']);      else $t_color2 = 'black';
+if (isset($_POST['t_text']))			$t_text = trim($_POST['t_text']);          else $t_text = 'white';
+if (isset($_POST['des_color']))			$des_color = trim($_POST['des_color']);    else $des_color = 'black';
+if (isset($_POST['des_text']))			$des_text = trim($_POST['des_text']);      else $des_text = 'white';
 
+if (isset($_POST['perk_name']))	       $perk_name = $_POST['perk_name'];     else $perk_name = Null;
+if (isset($_POST['perk_desc']))	       $perk_desc = $_POST['perk_desc'];     else $perk_desc = Null;
+
+$perk_pic = isset($_FILES['picture']['name']) ? $_FILES['picture']['name'] : array();
 
 if (isset($_POST['p']))			$p = trim($_POST['p']); else $p = NULL;
 if (isset($_POST['addd'])) {
@@ -80,24 +96,107 @@ echo " <div class='club_make'>";
 echo "    <div class='add_club_info'> <form action='create_club.php' method='post' enctype='multipart/form-data'>
 <table width='550' align='center' style='background-color: #FAF0E6'  cellpadding='4'>
 <tr><td width='30%'>Club Name</td><td ><input type='text' name='c_name' value='$c_name'  size='40'></td>
+<tr><td>Background color </td><td><input type='color' name='t_color1' value='$t_color1' >       
+<input type='color' name='t_color2' value='$t_color2' ></td>
+<tr><td>Text color </td><td><input type='color' name='t_text' value='$t_text' ></td>
+
 <tr><td>Club Tag </td><td><input type='text' name='c_tag' value='$c_tag' rows='10'  size='50'></td>
 <tr><td>Club Description</td><td><textarea name='c_desc' value='$c_desc' size='500' cols='40' rows='10' >$c_desc</textarea></td>
+<tr><td>Background color </td><td><input type='color' name='des_color' value='$des_color' ></td>
+<tr><td>Text color </td><td><input type='color' name='des_text' value='$des_text' ></td>
+
 <tr><td>Club Members</td><td><input type='number' name='c_members' value='$c_members'   size='12'></td>
 <tr><td>Upload Club Photo(JPG, GIF, PNG or TIF File only):
-        <td> <input type='file' name='image' ></td>";
-        $max_entries = 2;
-         if (isset($p_name[$i])) {               $perk_name = $p_name[$i]; }         else { $perk_name = null;}
-            if (isset($p_desc[$i])) {               $perk_desc = $p_desc[$i]; }         else { $perk_desc = null;}
-            if (isset($p_pic[$i])) {                $perk_pic = $p_pic[$i]; }           else { $perk_pic = null;}
-        // Loop through the number of entries
-        for ($i = 1; $i <= $max_entries; $i++) {
-           
+        <td> <input type='file' name='image' >$file_name</td></tr>
+        <tr><td></td>
+        <td><br></td>
+        </tr>";
+        $max_entries = 4;
+        for ($i = 0; $i < $max_entries; $i++) {
+            $per_pic = isset($perk_pic[$i]) ? $perk_pic[$i] : '';
             echo "
-<tr>  <td>Perk No $i</td>
-<tr><td>Perk pic </td><td> <input type='text' name='p_pic[]' value='$perk_pic'  size='50'></td>
-<tr><td>Perk Name </td><td><input type='text' name='p_name[]'  value=' $perk_name'  size='50'></td>
-<tr><td>Perk information </td><td><input type='text' name='p_desc[]' value=' $perk_desc' size='50'></td>";
+            <tr>
+                <td>Perk No " . ($i+1) . "</td>
+            </tr>
+            <tr>
+                <td>Perk Pic</td>
+                <td><input type='file' name='perk_pic[]' value='" . $per_pic . "' size='50'></td>
+            </tr>
+            <tr>
+                <td>Perk Name</td>
+                <td><input type='text' name='perk_name[]' value='" . (isset($perk_name[$i]) ? $perk_name[$i] : '') . "' size='50'></td>
+            </tr>
+            <tr>
+                <td>Perk Description</td>
+                <td><input type='text' name='perk_desc[]' value='" . (isset($perk_desc[$i]) ? $perk_desc[$i] : '') . "' size='50'></td>
+            </tr>
+            ";
         }
+
+        echo "  <tr><td></td>
+        <td><br></td>
+        </tr>";
+
+
+        if(isset($_FILES['picture'])) {
+            $max_entry = 3; // Number of entries to loop through
+            $S_title = $_POST['S_title']; // Assuming S_title is an array of values
+            $S_des = $_POST['S_des']; // Assuming S_des is an array of values
+           // $S_pic = $_POST['S_pic']; // Assuming S_pic is an array of values
+            $S_pic = isset($_FILES['picture']['name']) ? $_FILES['picture']['name'] : array(); // Assuming S_pic is an array of file names
+
+            for ($i = 0; $i < $max_entry; $i++) {
+                // Check if the array values are set, otherwise set them to null
+                $Slide_title = isset($S_title[$i]) ? $S_title[$i] : null;
+                $Slide_des = isset($S_des[$i]) ? $S_des[$i] : null;
+                $Slide_pic = isset($S_pic[$i]) ? $S_pic[$i] : null;
+        
+                // Upload file for each entry
+                $file_na = $_FILES['picture']['name'][$i];
+                $file_tmp = $_FILES['picture']['tmp_name'][$i];
+                $file_type = $_FILES['picture']['type'][$i];
+                $file_parts = explode('.', $file_na);
+                $file_ext = strtolower(end($file_parts));
+        
+                $valid_exts = array("jpg", "jpeg", "png", "tif", "gif");
+
+                if(in_array($file_ext, $valid_exts)) {
+                    $upload_path = "./upload/club_page/" . $file_na;
+                    move_uploaded_file($file_tmp, $upload_path);
+                    echo "File uploaded successfully for Slide No " . ($i+1) . ".<br>";
+                } else {
+                    $file_na = null;
+                    echo "Invalid file type. JPG, GIF, PNG or TIF files are allowed for Slide No " . ($i+1) . ".<br>";
+                }
+            }
+        }
+
+
+
+$max_entry = 3;
+for ($i = 0; $i < $max_entry; $i++) {
+    $Slide_pic = isset($S_pic[$i]) ? $S_pic[$i] : '';
+    echo "
+    <tr>
+        <td>Slide No " . ($i+1) . "</td>
+    </tr>
+    <tr>
+        <td>Slide Title</td>
+        <td><input type='text' name='S_title[]' value='" . (isset($S_title[$i]) ? $S_title[$i] : '') . "' size='50'></td>
+    </tr>
+    <tr>
+        <td>Slide Description</td>
+        <td><input type='text' name='S_des[]' value='" . (isset($S_des[$i]) ? $S_des[$i] : '') . "' size='50'></td>
+    </tr>
+    <tr>
+        <td>Slide Pic</td>
+        <td><input type='file' name='picture[]' value='" . $Slide_pic . "' size='50'></td>
+    </tr>";
+}
+
+
+
+
         echo "
 <tr><td></td><td><input type='submit' name='task' value='Finish' style=' font-size: 15px; margin: 1px 10px; display:inline-block; padding: 5px; border: 2px solid black;' size='08'>
 <input type='submit' name='task' value='preview' style=' font-size: 15px; margin: 1px 10px; display:inline-block; padding: 5px; border: 2px solid black;' size='08'></td>
@@ -121,8 +220,10 @@ case "signup":
                 echo"goooood"; break;
 case "Finish": 	    include('FSC_connect.php');
 
-                    $query ="INSERT INTO `club_page` (`c_name`, `c_tag`, `c_desc`, `c_pic`, `c_members`) 
-                    VALUES ( '$c_name', '$c_tag', '$c_desc', '$file_name', '$c_members')";
+                    $query ="INSERT INTO `club_page` (`c_name`, `c_tag`, `c_desc`, `c_pic`,
+                     `c_members`, `t_color1`, `t_color2`, `t_text`, `des_color`, `des_text`) 
+                    VALUES ( '$c_name', '$c_tag', '$c_desc', '$file_name', '$c_members',
+                     '$t_color1', '$t_color2', '$t_text', '$des_color', '$des_text')";
                     $result = mysqli_query($mysqli, $query);
                     if ($result) { $club_id = mysqli_insert_id($mysqli); echo"Your NEW Club Created. $club_id";
                     
@@ -139,17 +240,27 @@ case "Finish": 	    include('FSC_connect.php');
                     $query2 ="INSERT INTO `club_perks` (`perk_id`, `p_name`, `p_desc`, `p_pic`, `club_id`, `color`) 
                     VALUES (NULL,  '$_name', '$_desc', '$_pic', '$club_id', NULL)";
                     $result2 = mysqli_query($mysqli, $query2);
-                    if ($result2) echo"Your NEW Club Created.";
-                    else { echo"Unable to Make Account" . mysqli_error($mysqli);}}
-                    
+                    if ($result2) echo"Your perk Created.";
+                    else { echo"Unable to add perk" . mysqli_error($mysqli);}}
+
+
+                    for ($i = 0; $i < $max_entry; $i++) {
+                        // Check if the array values are set, otherwise set them to null
+                        $Slide_title = isset($S_title[$i]) ? $S_title[$i] : null;
+                        $Slide_des = isset($S_des[$i]) ? $S_des[$i] : null;
+                        $Slide_pic = isset($S_pic[$i]) ? $S_pic[$i] : null;
+                
+                    $query3 ="INSERT INTO `slide_pic` (`slide_id`, `S_title`, `S_des`, `S_pic`, `club_id`)
+                    VALUES (NULL,  '$Slide_title', '$Slide_des', '$Slide_pic', '$club_id')";
+                    $result3 = mysqli_query($mysqli, $query3);
+                    if ($result3) echo"Your slideshow pic Created.";
+                    else { echo"Unable to add slideshow" . mysqli_error($mysqli);}}
                     
                     
                     
                     }
                     else { echo"Unable to Make Account" . mysqli_error($mysqli);}
                     
-/*
-                    */
                     echo"ooooook"; break;
 
 case "First": break;
@@ -167,13 +278,13 @@ case "preview":
                 </div>
             </div>   </div></div>
  <section>
-            <div class='top'>
+            <div class='top'  style='background-image:radial-gradient($t_color1 40%, $t_color2);' >
                 <div class='imageHeader'>
                     <img style='width:100%; height: 100px; object-fit: cover;' src=\"./upload/club_page/$file_name\"  alt=\"Avatar\" alt=''>
                 </div>
             <div class='nametag'>
-            <h1>$c_name</h1>
-            <p> $c_tag</p>
+            <h1 style='color:$t_text;' >$c_name</h1>
+            <p  style='color:$t_text;' > $c_tag</p>
             </div>
             <div class='buttonSection'>
                 <div >
@@ -183,35 +294,73 @@ case "preview":
         </div>
 
 
-        <div class='clubInfo'>
-            <p>$c_desc</p>
+        <div class='clubInfo'  style='background-color:$des_color;'>
+            <p  style='color: $des_text;' >$c_desc</p>
         </div> 
         <div class='listOfBenefitsGrid'>";
 
+                for ($i = 0; $i < $max_entries; $i++) {
+                    // Check if the array values are set, otherwise set them to null
+                    $Per_pic = isset($perk_pic[$i]) ? $perk_pic[$i] : null;
+                    $Per_name = isset($perk_name[$i]) ? $perk_name[$i] : null;
+                    $Per_desc = isset($perk_desc[$i]) ? $perk_desc[$i] : null;
+            
+           echo"
 
-            $pics = $_POST['p_pic'];
-            $names = $_POST['p_name'];
-            $descs = $_POST['p_desc'];
-            
-            $products = array_combine(range(1, count($pics)), array_map(null, $pics, $names, $descs));
-            
-            foreach ($products as $id => $product) {
-                $_pic = $product[0];
-                $_name = $product[1];
-                $_desc = $product[2];
-            echo "
-       
             <div class='listOfBenefits'>
                 <div class='benefitsIcon'>
-                    <img src='./ClubHomePage/ClubHomePagePictures/network-icon.jpg' alt='$_pic'>
+                    <img src='./ClubHomePage/ClubHomePagePictures/network-icon.jpg' alt='$Per_pic'>
                 </div>
                 <div class='listOfBenefitsDesciption'>
-                    <h3>$_name</h3>
-                    <p>$_desc</p>
+                    <h3>$Per_name</h3>
+                    <p>$Per_desc</p>
                 </div>
             </div>  ";}
 
-            echo"</div> </section>  ";  break;
+            echo"</div> </section>  
+            
+            <section> 
+            <div class='slideshow-container'>";
+
+                for ($i = 0; $i < $max_entry; $i++) {
+                    // Check if the array values are set, otherwise set them to null
+                    $Slide_title = isset($S_title[$i]) ? $S_title[$i] : null;
+                    $Slide_des = isset($S_des[$i]) ? $S_des[$i] : null;
+                    $Slide_pic = isset($S_pic[$i]) ? $S_pic[$i] : null;
+           echo"
+
+            <div class='mySlides fade'>
+			<div class='numbertext'> " . ($i+1) . "</div>
+			<img src=\"./upload/club_page/$Slide_pic\" style='width:100%'>
+			<div class='text'>$Slide_title<br>$Slide_des</div>
+			</div>
+            ";}
+            echo"	
+            <div class='prev' onclick='plusSlides(-1)'>❮</div>
+			<div class='next'  onclick='plusSlides(1)'>❯</div>
+			<br>
+			<div class='do' style='text-align:center'>
+				<span class='dot' onclick='currentSlide(1)'></span> 
+				<span class='dot' onclick='currentSlide(2)'></span> 
+				<span class='dot' onclick='currentSlide(3)'></span> 
+			</div>
+
+</div>
+<br>
+
+
+		
+
+<script src='autoSlide.js'></script>
+<script src='clickSlide.js'></script>
+
+</section>
+            
+            
+            
+            
+            
+            ";  break;
 
 case "ad_club": 
    
