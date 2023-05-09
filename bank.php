56a74c9b72660eb1779b411e18e3b9635891e477
Krrishman <you@example.com>
2023-05-09 08:45:03 -0400

    zxczsczcxc

--- bank.php
+++ bank.php
@@ -239,7 +239,7 @@ switch($task) {
                                     else { echo"Deposit Failed\n" . pg_last_error($conn);}}
 
                                     else { echo"Deposit Failed" . pg_last_error($conn);}
-				
+                                    header("Refresh:0");
 						break;
 	case "withdraw":
 						echo "    <form action='update.php' method='post'>
