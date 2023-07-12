b1b1094b6daf424299eec11ecafea79af0f60bf0
Krrishman <you@example.com>
2023-07-12 19:00:23 -0400

    zxczxczxc

--- profile.php
+++ profile.php
@@ -14,13 +14,14 @@
 	<html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link rel="stylesheet" href="profile.css">
 		<link rel="stylesheet" href="footer.css">
 		<style>
 		#ma{
         display: flex;
         justify-content: center;
-        width: 100%;
+        width: 300px;
         background-color: #FAF0E6;
         padding: 10px;
         margin: 10px;
